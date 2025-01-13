fbd76667dcb92d2bae54d3bc75397ab3e1abfd25
VladimirA1995 <wolfelgor11@gmail.com>
2025-01-13 11:03:53 +0300

    Add files via upload

---
+++ stag1.1.py
@@ -0,0 +1,12 @@
+use_name = 'Владимир'
+print(use_name)
+title = 'Переменные'
+print(title)
+content = 'Типы переменных'
+print(content)
+status = 'На проверке'
+print( status)
+created_date = '13-01-2025'
+print(created_date)
+issue_date = '13-02-2025'
+print(issue_date)
